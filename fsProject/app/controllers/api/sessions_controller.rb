c5643be4949dd59d52107d681a0b0fff919555dc
jdkeds <larastar721@gmail.com>
2021-02-08 13:47:10 -0800

    Finishing #create and #destroy for SessionsController to login and logout a user

--- fsProject/app/controllers/api/sessions_controller.rb
+++ fsProject/app/controllers/api/sessions_controller.rb
@@ -1,2 +1,22 @@
 class Api::SessionsController < ApplicationController
+
+    def create
+        @user = User.find_by_credentials(params[:user][:username],
+        params[:user][:password])
+        if !@user
+            render json: ['Incorrect username or password!'], status: 401
+        else    
+            login(@user)
+            render 'api/users/show'
+        end
+    end
+
+    def destroy
+        if current_user
+            logout!
+            render json: {}
+        else
+            render json:['No one is logged in'], status: 404
+        end
+    end
 end
