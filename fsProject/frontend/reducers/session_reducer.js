d9e17389547a800809f309637038f7d53921dfff
jdkeds <larastar721@gmail.com>
2021-02-08 14:50:15 -0800

    Creating reducers to handle session actions

---
+++ fsProject/frontend/reducers/session_reducer.js
@@ -0,0 +1,19 @@
+import { LOGOUT_CURRENT_USER, RECEIVE_CURRENT_USER } from "../actions/session_actions";
+
+
+const sessionReducer = (state = {currentUserId: null}, action) => {
+    Object.freeze(state);
+    let newState = Object.assign({}, state);
+    switch (action.type) {
+        case RECEIVE_CURRENT_USER:
+            newState[currentUserId] = user.id;
+            return newState;
+        case LOGOUT_CURRENT_USER:
+            newState[currentUserId] = null;
+            return newState;
+        default:
+            return state;
+    }
+}
+
+export default sessionReducer;
