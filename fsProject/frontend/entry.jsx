dd0bdb3274b6515999267808ad545fc514c56c57
jdkeds <larastar721@gmail.com>
2021-02-08 15:27:40 -0800

    Setting up root and app react components

--- fsProject/frontend/entry.jsx
+++ fsProject/frontend/entry.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
+import Root from "./components/root";
 import * as Action from './actions/session_actions';
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
     const store = configureStore();
-    ReactDOM.render(<h1>Flickr Clone</h1>, root);
+    ReactDOM.render(<Root store={store}/>, root);
 
     //Remove these after done testing!
     window.store = store;
-    window.login = Action.login;
-    window.logout = Action.logout;
-    window.signup = Action.signup;
+    // window.login = Action.login;
+    // window.logout = Action.logout;
+    // window.signup = Action.signup;
 })
