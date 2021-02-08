05a0ed2b3efd9fa830db4282d1cddf01e151a7d3
jdkeds <larastar721@gmail.com>
2021-02-08 13:40:38 -0800

    Adding ApplicationController methods for logged in users

--- fsProject/app/controllers/application_controller.rb
+++ fsProject/app/controllers/application_controller.rb
@@ -1,2 +1,30 @@
 class ApplicationController < ActionController::Base
+    helper_method :curret_user, :logged_in?
+
+    def login(user)
+        session[:session_token] = user.session_token
+    end
+
+    def current_user
+        return nil unless session[:session_token]
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
+    def logout!
+        current_user.rese_session_token!
+        session[:session_token] = nil
+        @current_user = nil
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def require_logged_out
+        redirect_to user_url(current_user) if logged_in?
+    end
+
+    def require_logged_in
+        redirect_to new_session_url unless logged_in?
+    end
 end
