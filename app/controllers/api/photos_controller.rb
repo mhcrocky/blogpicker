60042220d6cfb9e28644d7fdebc742d146b831f7
jdkeds <larastar721@gmail.com>
2021-02-11 10:12:21 -0800

    Writing crud methods for photos

--- app/controllers/api/photos_controller.rb
+++ app/controllers/api/photos_controller.rb
@@ -3,12 +3,39 @@ class Api::PhotosController < ApplicationController
         @photo = Photo.new(photo_params)
         @photo.user_id = current_user.id
         if @photo.save
-            redirect_to 
+            redirect_to api_photo(@photo)
         else  
             render json: @photo.errors.full_messages, status: 422
         end
     end
 
+    def update
+        @photo = Photo.find_by(id: params[:id])
+        if @photo.update(photo_params)
+            redirect_to api_photo(@photo)
+        else
+            render json: @photo.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy
+        photo = Photo.find_by(id: params[:id])
+        photo.delete
+    end
+
+    def index
+        @photos = Photo.all 
+        render :index
+    end
+
+    def show
+        @photo = Photo.find_by(id: params[:id])
+        if @photo
+            render :show
+        else
+            render json: @photo.errors.full_messages, status: 404
+        end
+    end
 
     private
 
