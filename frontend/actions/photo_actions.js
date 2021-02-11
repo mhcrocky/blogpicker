f5b89ee757f9a4d40c5c8e829fb18ba5e50ed36b
jdkeds <larastar721@gmail.com>
2021-02-11 11:40:32 -0800

    Writing action and thunk action creators for photos

---
+++ frontend/actions/photo_actions.js
@@ -0,0 +1,65 @@
+import * as APIUtil from '../util/photo_api_util';
+
+export const RECEIVE_ALL_PHOTOS = 'RECEIVE_ALL_PHOTOS';
+export const RECEIVE_PHOTO = 'RECEIVE_PHOTO';
+export const DELETE_PHOTO = 'DELETE_PHOTO';
+
+//regular actions
+
+const receiveAllPhotos = (photos) => {
+    return {
+        type: RECEIVE_ALL_PHOTOS,
+        photos
+    }
+}
+
+const receivePhoto = (photo) => {
+    return {
+        type: RECEIVE_PHOTO,
+        photo
+    }
+}
+
+const removePhoto = (photoId) => {
+    return {
+        type: DELETE_PHOTO,
+        photoId
+    }
+}
+
+//thunks
+
+export const createPhoto = (photo) => (dispatch) => {
+    return APIUtil.createPhoto(photo)
+        .then(photo => {
+            dispatch(receivePhoto(photo));
+        })
+}
+
+export const fetchAllPhotos = () => (dispatch) => {
+    return APIUtil.fetchAllPhotos()
+        .then(photos => {
+            dispatch(receiveAllPhotos(photos));
+        })
+}
+
+export const fetchPhoto = (photoId) => (dispatch) => {
+    return APIUtil.fetchPhoto(photoId)
+        .then(photo => {
+            dispatch(receivePhoto(photo));
+        })
+}
+
+export const updatePhoto = (photo) => (dispatch) => {
+    return APIUtil.updatePhoto(photo)
+        .then(photo => {
+            dispatch(receivePhoto(photo));
+        })
+}
+
+export const deletePhoto = (photoId) => (dispatch) => {
+    return APIUtil.deletePhoto(photoId)
+        .then((photo) => {
+            dispatch(removePhoto(photo.id))
+        })
+}
