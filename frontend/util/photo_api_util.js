6f2daff4d0fecb16341f607507b7d93863371b7f
jdkeds <larastar721@gmail.com>
2021-02-11 11:17:48 -0800

    Creating photo_api_util for ajax requests on the frontend

---
+++ frontend/util/photo_api_util.js
@@ -0,0 +1,36 @@
+export const createPhoto = (photo) => {
+    return $.ajax({
+        url: '/api/photos',
+        method: 'POST',
+        data: { photo }
+    })
+}
+
+export const fetchAllPhotos = () => {
+    return $.ajax({
+        url: '/api/photos',
+        method: 'GET'
+    })
+}
+
+export const fetchPhoto = (photoId) => {
+    return $.ajax({
+        url: `/api/photos/${photoId}`,
+        method: 'GET'
+    })
+}
+
+export const updatePhoto = (photo) => {
+    return $.ajax({
+        url: `/api/photos/${photo.id}`,
+        method: 'PATCH',
+        data: { photo }
+    })
+}
+
+export const deletePhoto = (photoId) => {
+    return $.ajax({
+        url: `/api/photos/${photoId}`,
+        method: 'DELETE'
+    })
+}
