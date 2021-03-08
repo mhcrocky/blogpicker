e178c5b3ccd425c72a7a778eeed53186d5d90459
jdkeds <larastar721@gmail.com>
2021-03-08 14:17:44 -0800

    Working on CommentForm react component

--- frontend/components/comment_form/comment_form.jsx
+++ frontend/components/comment_form/comment_form.jsx
@@ -1,7 +1,30 @@
 class CommentForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {body: ""};
+        this.inputHandler = this.inputHandler.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    inputHandler(e) {
+        this.setState({body: e.target.value});
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        let data = {body: this.state.body, photo_id: this.props.photoId};
+        this.props.createComment(data);
+    }
+
     render() {
         return(
-            <div></div>
+            <div className="comment-form-container">
+                <form onSubmit={this.handleSubmit} className="comment-form">
+                    <input onChange={this.inputHandler} type="text"
+                    value={this.state.body} placeholder="Add a comment"/>
+                    <button className="upload-button">Comment</button>
+                </form>
+            </div>
         )
     }
 }
