18b95f68b0bb8e41347eabd8a11c552f60439062
jdkeds <larastar721@gmail.com>
2021-03-11 17:08:42 -0800

    Adding rails specific meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,4 +27,8 @@ class ApplicationController < ActionController::Base
     def require_logged_in
         redirect_to new_session_url unless logged_in?
     end
+
+    def default_url_options
+        { host: ENV["DOMAIN"] || "localhost:3000" }
+    end 
 end
