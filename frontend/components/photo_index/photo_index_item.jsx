aa712ff7d1dfb1bde66625240949ad445c787748
jdkeds <larastar721@gmail.com>
2021-03-12 11:57:13 -0800

    Converting photo_index_item into a functional component

--- frontend/components/photo_index/photo_index_item.jsx
+++ frontend/components/photo_index/photo_index_item.jsx
@@ -1,25 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-class PhotoIndexItem extends React.Component {
+const PhotoIndexItem = (props) => {
+    const photo = props.photo;
+    const username = props.user.username;
 
-    render() {
-        const photo = this.props.photo;
-        const username = this.props.user.username;
-
-        return (
-            <li className="photo-item-container">
-                <div className="photo-details">
-                    <div>{photo.title}</div>
-                    <div>by {username}</div>
-                </div>
-                <Link to={`/photos/${photo.id}`}>
-                    <img loading="lazy" src={photo.photoUrl} alt={photo.title}/>
-                </Link>
-            </li>
-
-        )
-    }
+    return (
+        <li className="photo-item-container">
+            <div className="photo-details">
+                <div>{photo.title}</div>
+                <div>by {username}</div>
+            </div>
+            <Link to={`/photos/${photo.id}`}>
+                <img loading="lazy" src={photo.photoUrl} alt={photo.title}/>
+            </Link>
+        </li>
+    )
 }
 
 export default PhotoIndexItem;
